ea90fe6bfa109a20a391076c9960dd02d208fc25
Trung Nguyen <ttrungnguyenhp.2017@gmail.com>
2024-09-08 13:14:42 -0400

    added printing of options to main

--- program.c
+++ program.c
@@ -1,5 +1,10 @@
 #include <stdio.h>
 #include "program.h"
 int main(){
-    printf("Hello World\n");
+    printf("1. add an student to the end of the list after prompting for all the student's data \n"
+            "2. delete all students with a given last name from the list (pay particular attention to what happens if you delete the first or last student!) \n"
+            "3. print the list from beginning to end \n"
+            "4. print the list from end to beginning \n"
+            "5. exit the program. \n" );
+
 }
